8f9ab6392159b70838f2ad738871301372c7bc88
Richard Xu <richard@Richards-MacBook-Pro.local>
2018-05-17 20:38:47 +0800

    implement cart: 1.) create tree; 2.) prune tree;

---
+++ decisionTrees/cart.py
@@ -0,0 +1,471 @@
+# -*- coding: utf8 -*-
+
+import copy
+import numpy as np
+from sklearn import tree as SKTREE
+from sklearn.externals.six import StringIO
+from sklearn.datasets import load_iris
+import graphviz
+
+NODE = 'node'
+LEAF = 'leaf'
+CLF = "classifier"
+REG = "regression"
+DISCRETE = "discrete"
+CONTINUOUS = "continuous"
+COMPARATOR = {DISCRETE: np.equal, CONTINUOUS: np.less_equal}
+
+
+def plotDTree(dTree, treeName):
+    '''
+    Visualize decision tree
+    '''
+    dot_data = StringIO()
+    dot_data.write("graph dtree {\n")
+    nodeList = [dTree]
+    nodeIdxList = [0]
+    while len(nodeList) > 0:
+        latestIdx = nodeIdxList[-1]
+
+        node = nodeList[0]
+        del nodeList[0]
+        nodeIdx = nodeIdxList[0]
+        del nodeIdxList[0]
+
+        if node['type'] == LEAF:
+            nodeLabel = "gini: %.3f\nforecast: %d" % (node['gini'], node['fcVal'])
+            dot_data.write("%d [label=\"%s\" shape=box]" % (nodeIdx, nodeLabel))
+        else:
+            opName = "<=" if node['comparator'] == np.less_equal else "=="
+            nodeLabel = "X[%d]%s%s\ngini: %.3f" % (node['splitFeature'], 
+                    opName, node['splitValue'], node['gini'],)
+            dot_data.write("%d [label=\"%s\" shape=box]" % (nodeIdx, nodeLabel))
+
+        for key in ['left', 'right']:
+            branch = node.get(key, None)
+            if branch:
+                latestIdx += 1
+                nodeList.append(branch)
+                nodeIdxList.append(latestIdx)
+                dot_data.write("%d -- %d\n" % (nodeIdx, latestIdx))
+    dot_data.write("}")
+
+    filename = "./trees/%s.png" % treeName 
+    graph = graphviz.Source(dot_data.getvalue(), filename=filename, format="png")
+    graph.view()
+
+
+def giniImpurity(dl):
+    '''
+    Calculate Gini Impurity.
+
+    Parameters
+    ----------
+    dl : array
+        data label vector
+
+    Returns
+    ----------
+    Gini Impurity which is calculated on dl.
+    '''
+    labelSet = set(dl)
+    gini = 1.
+    for label in labelSet:
+        count = np.count_nonzero(dl == label)
+        gini -= (float(count) / dl.size) ** 2
+    return gini
+
+
+def clfSplitPoint(df, dl, ft):
+    '''
+    Choose the best split point for classifier with gini impurity as metric.
+
+    Parameters
+    ----------
+    df : array
+        data feature matrix
+    dl : array
+        data label vector
+    ft : list
+        feature type (DISCRETE / CONTINUOUS)
+
+    Returns
+    ----------
+    splitFeature : int
+        Index of feature which is the best one to be splitted.
+    splitValue : float
+        It is the best split point on feature splitFeature.
+    minGini : float
+        gini impurity after splitting
+    '''
+    minGini = np.inf
+    splitFeature = -1
+    splitValue = -1
+    splitLeftIdxList = None
+    splitRightIdxList = None
+    m, n = df.shape
+    idxList = np.arange(m)
+    for feat in range(n):
+        comparator = COMPARATOR[ft[feat]]
+        for val in set(df[:, feat]):
+            curGini = 0.
+
+            # left branch
+            leftIdxList = np.where(comparator(df[:, feat], val))[0]
+            if leftIdxList.size > 0:
+                curGini += giniImpurity(dl[leftIdxList]) * leftIdxList.size
+
+            # right branch
+            rightIdxList = np.array([i for i in idxList if i not in leftIdxList])
+            if rightIdxList.size > 0:
+                curGini += giniImpurity(dl[rightIdxList]) * rightIdxList.size
+
+            if curGini < minGini:
+                minGini = curGini
+                splitFeature = feat
+                splitValue = val
+                splitLeftIdxList = leftIdxList
+                splitRightIdxList = rightIdxList
+
+    if ft[splitFeature] == CONTINUOUS:
+        # optimize split point
+        pass
+
+    return splitFeature, splitValue, minGini, comparator, splitLeftIdxList, splitRightIdxList
+
+
+def clfCreateNode(df, dl, ft):
+    '''
+    Create tree node for classifier.
+
+    Parameters
+    ----------
+    df : array
+        data feature matrix
+    dl : array
+        data label vector
+    ft : list
+        feature type (DISCRETE / CONTINUOUS)
+
+    Returns
+    ----------
+    node : dict
+        A dictionary represents tree node or leaf.
+        {
+        'type': string node / leaf
+        'gini': float gini impurity
+        'fcVal' : int forecasted class
+        'splitFeature' : int splitting feature
+        'splitValue' : float splitting value
+        'comparator' : operator on splitFeature
+        'left' : node contains data which splitFeature is equal to splitValue
+        'right': node contains data which splitFeature is not equal to splitValue
+        'giniError' : float decrease of impurity after splitting, used for pruning
+        }
+    '''
+    node = {'type': LEAF}
+    node['gini'] = giniImpurity(dl)
+    # set the most label as current node label
+    labels = list(set(dl))
+    countList = [np.count_nonzero(dl == label) for label in labels]
+    node['fcVal'] = labels[np.argmax(countList)]
+
+    # check end condition (implement prepruning here)
+    if len(labels) == 1:
+        return node
+
+    # create subnodes
+    node['type'] = NODE
+    splitFeature, splitValue, splitGini, comparator, leftIdxList, \
+            rightIdxList = clfSplitPoint(df, dl, ft)
+    node['splitFeature'] = splitFeature
+    node['splitValue'] = splitValue
+    node['comparator'] = comparator
+    node['giniError'] = node['gini'] * dl.shape[0] - splitGini
+
+    # create left subnode
+    if leftIdxList.size > 0:
+        node['left'] = clfCreateNode(df[leftIdxList], dl[leftIdxList], ft)
+
+    # create right subnode
+    if rightIdxList.size > 0:
+        node['right'] = clfCreateNode(df[rightIdxList], dl[rightIdxList], ft)
+
+    return node
+
+
+def findLeafParents(node, parentNodes):
+    '''
+    查找carTree上所有叶子结点的父节点
+    '''
+    left = node['left']
+    right = node['right']
+
+    if left['type'] == LEAF and right['type'] == LEAF:
+        parentNodes.append(node)
+        return
+
+    if left['type'] == NODE:
+        findLeafParents(node.get('left'), parentNodes)
+
+    if right['type'] == NODE:
+        findLeafParents(node.get('right'), parentNodes)
+
+
+def pruneOneBranch(carTree):
+    '''
+    对分类树进行剪枝，剪去carTree上误差增益最小的分支
+    '''
+    leafParents = []
+    findLeafParents(carTree, leafParents)
+    alpha = np.inf
+    prunedNode = None
+    for parentNode in leafParents:
+        giniError = parentNode.get('giniError')
+        if giniError < alpha:
+            alpha = giniError
+            prunedNode = parentNode
+    if prunedNode:
+        prunedNode['type'] = LEAF
+        del prunedNode['splitFeature']
+        del prunedNode['splitValue']
+        del prunedNode['comparator']
+        del prunedNode['left']
+        del prunedNode['right']
+        del prunedNode['giniError']
+
+
+def genSubTreeList(carTree, saveTree=False):
+    '''
+    剪枝，生成子树列表
+    '''
+    treeList = [carTree]
+    subTree = carTree
+    while subTree['type'] != LEAF:
+        subTree = copy.deepcopy(subTree)
+        pruneOneBranch(subTree)
+        treeList.append(subTree)
+ 
+    if saveTree:
+        for i in range(len(treeList)):
+            plotDTree(treeList[i], "tree%d" % i)
+
+    return treeList
+
+
+def regSplitPoint(df, dl, fs):
+    '''
+    Choose the best split point for regression with the least square error as metric.
+
+    Parameters
+    ----------
+    df : array
+        data feature matrix
+    dl : array
+        data label vector
+    fs : list
+        feature space vector
+
+    Returns
+    ----------
+    splitFeature : int
+        Index of feature which is the best one to be splitted.
+    splitValue : float
+        It is the best split point on feature splitFeature.
+    '''
+    squareError = np.inf
+    splitFeature = -1
+    splitValue = -1
+    for feat in fs:
+        for val in set(df[:, feat]):
+            curSqureError = 0.
+            # greater than branch
+            idxList = np.where(df[:, feat] > val)
+            if idxList.size > 0:
+                curSqureError += np.var(dl[idxList]) * idxList.size
+            # less than or equal branch
+            idxList = np.where(df[:, feat] <= val)
+            if idxList.size > 0:
+                curSqureError += np.var(dl[idxList]) * idxList.size
+
+            if curSqureError < squareError:
+                squareError = curSqureError
+                splitFeature = feat
+                splitValue = val
+    return splitFeature, splitValue
+
+
+def regCreateNode(df, dl, fs):
+    '''
+    Create tree node for regression.
+
+    Parameters
+    ----------
+    df : array
+        data feature matrix
+    dl : array
+        data label vector
+    fs : list
+        feature space vector
+
+    Returns
+    ----------
+    node : dict
+        A dictionary represents tree node or leaf.
+        {
+        'type': string node / leaf
+        'fcVal' : int forecasted class
+        'splitFeature' : int splitting feature
+        'splitValue' : float splitting value
+        'left' : node contains data which splitFeature is greater than splitValue
+        'right': node contains data which splitFeature is less than or equal to splitValue
+        }
+
+    '''
+    node = {'type': LEAF}
+    # set label mean as current forecasted value
+    node['fcVal'] = np.mean(dl)
+
+    # check end condition
+    if fs.size == 0 or np.var(dl) == 0.:
+        return node
+
+    node['type'] = NODE
+    # choose the best splitting point
+    splitFeature, splitValue = regSplitPoint(df, dl, fs)
+    node['splitFeature'] = splitFeature
+    node['splitValue'] = splitValue
+
+    # create left subnode
+    idxList = np.where(df[:, splitFeature] > splitValue)[0]
+    if idxList.size > 0:
+        node['left'] = regCreateNode(df[idxList], dl[idxList],
+                fs[:].remove(splitFeature))
+
+    # create right subnode
+    idxList = np.where(df[:, splitFeature] <= splitValue)[0]
+    if idxList.size > 0:
+        node['right'] = regCreateNode(df[idxList], dl[idxList],
+                fs[:].remove(splitFeature))
+
+    return node
+
+
+def createTree(dataFeats, dataLabels, featTypes, ctType=CLF):
+    '''
+    Create decision tree for different scenario, classifier or regression.
+    '''
+    if ctType == REG:
+        createNode = regCreateNode
+    else:
+        createNode = clfCreateNode
+
+    dTree = createNode(dataFeats, dataLabels, featTypes)
+    return dTree
+
+
+def forecastData(dTree, data):
+    '''
+    Using dTree to forecast data with correlative comparator, np.equal for classifier or np.greater for regression.
+    '''
+    point = dTree
+    while True:
+        if point is None:
+            return None
+
+        if point['type'] == LEAF:
+            return point['fcVal']
+
+        comparator = point['comparator']
+        splitFeature = point['splitFeature']
+        splitValue = point['splitValue']
+
+        if comparator(data[splitFeature], splitValue):
+            point = point.get('left', None)
+        else:
+            point = point.get('right', None)
+    return None
+
+
+def loadCarData():
+    '''
+    数据来源：Dua, D. and Karra Taniskidou, E. (2017). UCI Machine Learning Repository [http://archive.ics.uci.edu/ml]. Irvine, CA: University of California, School of Information and Computer Science.
+    [Car Evaluation] https://archive.ics.uci.edu/ml/datasets/Car+Evaluation
+    '''
+    dataset = []
+    featNames = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety']
+    with open('./car/car.data') as df:
+        for line in df:
+            fields = line.strip().split(',')
+            dataset.append(fields)
+    return np.array(dataset), featNames
+
+
+def testCart():
+    '''
+    测试决策树
+    '''
+#    dataSet, featNames = loadCarData()
+    iris = load_iris()
+    featTypes = [CONTINUOUS, CONTINUOUS, CONTINUOUS, CONTINUOUS]
+    dataSet = iris.data
+    labels = iris.target
+
+    dataIdxList = list(range(dataSet.shape[0]))
+    np.random.shuffle(dataIdxList)
+    trainCount = int(len(dataIdxList) * 2 / 3)
+    trainIdxList = dataIdxList[0:trainCount]
+    testIdxList = dataIdxList[trainCount:]
+    trainDataSet = dataSet[trainIdxList]
+    trainLabels = labels[trainIdxList]
+    testDataSet = dataSet[testIdxList]
+    testLabels = labels[testIdxList]
+
+    orgTree = createTree(trainDataSet, trainLabels, featTypes, CLF)
+    # pruning
+    treeList = genSubTreeList(orgTree)
+
+    # choose the best one with cross validation
+    minErrTreeIndex = -1
+    minErrRate = np.inf
+    for index in range(len(treeList)):
+        clfTree = treeList[index]
+        errnum = 0.
+        for i in range(testDataSet.shape[0]):
+            testData = testDataSet[i]
+            cls = forecastData(clfTree, testData)
+            if cls != testLabels[i]:
+                errnum += 1
+        errRate = errnum / testDataSet.shape[0]
+        if errRate < minErrRate:
+            minErrRate = errRate
+            minErrTreeIndex = index
+    print ('minErrTreeIndex = %d' % minErrTreeIndex)
+    print ('minErrRate is %f' % minErrRate)
+    plotDTree(treeList[minErrTreeIndex], "bestTree")
+
+    # compare with the decision tree built with sklearn.tree
+    useSkDT(trainDataSet, trainLabels, testDataSet, testLabels)
+
+
+
+def useSkDT(trainDataSet, trainLabels, testDataSet, testLabels):
+    '''
+    Using sklearn to build and test the decision tree with the same data
+    '''
+    skClf = SKTREE.DecisionTreeClassifier()
+    skClf = skClf.fit(trainDataSet, trainLabels)
+
+    dot_data = StringIO()
+    SKTREE.export_graphviz(skClf, out_file=dot_data) 
+    graph = graphviz.Source(dot_data.getvalue(), filename="./trees/skDTree.png", format="png")
+    graph.view()
+
+    predicts = skClf.predict(testDataSet)
+    testCount = testDataSet.shape[0]
+    skErrRate = float(np.count_nonzero(predicts != testLabels)) / testCount
+    print ('sklearn error rate is %f' % skErrRate)
+
+
+if __name__ == '__main__':
+    pass
--- decisionTrees/decisionTrees.py
+++ decisionTrees/decisionTrees.py
@@ -3,6 +3,10 @@ import operator
 import numpy as np
 
 
+ID3 = "decision_tree_ID3"
+C45 = "decision_tree_C45"
+
+
 def calcShannonEntropy(dataset):
     '''
     计算数据集的熵
@@ -20,31 +24,30 @@ def calcShannonEntropy(dataset):
 
 def splitDataSet(dataset, dim):
     '''
-    将数据集dataset在维度dim上进行划分
+    将数据集dataset在维度dim上进行划分，每一个取值对应一个分支
     @param dataset numpy.array
     '''
     vals = set(dataset[:, dim])
     subs = {}
     for val in vals:
-        sub_data_set = dataset[dataset[:, dim] == val]
-        subs[val] = np.column_stack((sub_data_set[:, :dim],
-            sub_data_set[:, dim+1:]))
+        subs[val] = dataset[dataset[:, dim] == val]
 
-    return subs        
+    return subs
 
 
-def chooseBestFeatureAndSplit(dataset):
+def chooseBestFeatureAndSplit(dataset, featIdxList, dtType):
     '''
-    选择最佳的划分特征，既信息增益最大的特征，然后进行划分
+    选择最佳的划分特征，即信息增益(ID3)或信息增益比(C45)最大的特征，然后进行划分
     @param dataset numpy.array
+    @param featIdxList list 还未参与分裂的特征索引
+    @param dtType str 决策树类型: ID3 / C4.5
     '''
     before_split_entropy = calcShannonEntropy(dataset)
     best_info_gain = 0.0
     best_dim = -1
     best_split = {}
 
-    dims = dataset.shape[1] - 1
-    for dim in range(dims):
+    for dim in featIdxList:
         after_split_entropy = 0.0
         subs = splitDataSet(dataset, dim)
         for val in subs.keys():
@@ -52,6 +55,10 @@ def chooseBestFeatureAndSplit(dataset):
             after_split_entropy += prob * calcShannonEntropy(subs[val])
 
         cur_info_gain = before_split_entropy - after_split_entropy
+        if dtType == C45:
+            # use information gain ratio for C45
+            cur_info_gain /= before_split_entropy
+
         if cur_info_gain > best_info_gain:
             best_info_gain = cur_info_gain
             best_dim = dim
@@ -67,75 +74,142 @@ def majorityVote(dataset):
     @param dataset numpy.array
     '''
     label2Count = {}
-    for label in dataset:
-        label2Count.setdefault(label, 0)
-        label2Count[label] += 1
+    labels = dataset[:, -1]
+    for label in set(labels):
+        label2Count[label] = np.count_nonzero(labels == label)
     sortedLabel2Count = sorted(label2Count.iteritems(),
             key=operator.itemgetter(1), reverse=True)
     return sortedLabel2Count[0][0]
 
 
-def createDecisionTreeID3(dataset, featNames):
+def createDecisionTree(dataset, featIdxList, dtType=ID3):
     '''
     创建决策树
     @param dataset numpy.array
-    @param featNames list 
+    @param featIdxList list 
     '''
 
     # 样本集中的标签已经统一了
     if dataset.shape[0] == np.count_nonzero(dataset[:, -1] == dataset[0][-1]):
-        return dataset[0][-1]
+        return {'leaf': True, 'dataset': dataset, 'cls': dataset[0][-1]}
 
     # 样本集的所有特征都已经检查过了，但是标签还不统一，需要进行多数票决
-    if len(dataset[0]) == 1:
-        return majorityVote(dataset)
+    if len(featIdxList) == 0:
+        return {'leaf': True, 'dataset': dataset, 'cls': majorityVote(dataset)}
 
     # 构建当前树
-    bestDim, bestSplit = chooseBestFeatureAndSplit(dataset)
-    bestFeatName = featNames[bestDim]
-    curTree = {bestFeatName: {}}
-    del featNames[bestDim]
+    bestDim, bestSplit = chooseBestFeatureAndSplit(dataset, featIdxList, dtType)
+    curTree = {'splitFeat': bestDim, 'dataset': dataset,
+            'cls': majorityVote(dataset), 'branches': {}}
+    featIdxList.remove(bestDim)
 
     # 构建子树
     for featval, subset in bestSplit.iteritems():
-        curTree[bestFeatName][featval] = createDecisionTreeID3(subset, featNames[:])
+        curTree['branches'][featval] = createDecisionTree(subset, featIdxList[:], dtType)
 
     return curTree
 
 
-def classify(decisionTree, featNames, testData):
+def classify(decisionTree, testData):
     '''
     使用决策树decisionTree对测试样本testData进行分类
     '''
-    decisionFeat = decisionTree.keys()[0]
-    nextLevel = decisionTree[decisionFeat]
-    decisionFeatIdx = featNames.index(decisionFeat)
+    # 获取当前节点的预测类别
+    testLabel = decisionTree.get('cls', 'unknown')
 
-    testLabel = 'unknown'
-    for featVal in nextLevel.keys():
-        if testData[decisionFeatIdx] == int(featVal):
-            if type(nextLevel[featVal]) is dict:
-                testLabel = classify(nextLevel[featVal], featNames, testData)
-            else:
-                testLabel = nextLevel[featVal]
+    # 如果到达叶子节点直接返回
+    if decisionTree.get('leaf', False):
+        return testLabel
+
+    # 沿着分支继续前进
+    branches = decisionTree.get('branches', {})
+    splitFeat = decisionTree.get('splitFeat', -1)
+
+    for featVal in branches.keys():
+        if testData[splitFeat] == featVal:
+            testLabel = classify(branches[featVal], testData)
 
     return testLabel
 
 
-def createTestDataSet():
-    dataSet = [[1, 1, 'yes'], 
-            [1, 1, 'yes'], 
-            [1, 0, 'no'], 
-            [0, 1, 'no'], 
-            [0, 1, 'no']] 
-    labels = ['no surfacing','flippers'] 
-    return dataSet, labels
+def pruneDT(node, alpha):
+    '''
+    剪枝
+    '''
+    # 如果到达叶子节点就计算自己的分类损失
+    if node.get('leaf', False):
+        # leaf node
+        dataset = node['dataset']
+        return dataset.shape[0] * calcShannonEntropy(dataset) + alpha
+
+    # 如果不是叶子节点就沿着各个分支继续前进
+    branches = node['branches']
+    costBefore = 0.0
+    leafNum = 0
+    for featVal in branches.keys():
+        leafCost = pruneDT(branches[featVal], alpha)
+        if leafCost is not None:
+            costBefore += leafCost
+            leafNum += 1
+
+    # 如果所有子节点都变成了叶子节点则继续修剪该节点
+    if leafNum == len(branches.keys()):
+        dataset = node['dataset']
+        costAfter = dataset.shape[0] * calcShannonEntropy(dataset) + alpha
+        # 如果修剪后分类的损失减小则修剪该节点，否则当前分支停止修剪
+        if costAfter < costBefore:
+            # prune current node
+            node['leaf'] = True
+            del node['branches']
+            del node['splitFeat']
+            return costAfter
+
+    return None
+
+def loadCarData():
+    '''
+    数据来源：Dua, D. and Karra Taniskidou, E. (2017). UCI Machine Learning Repository [http://archive.ics.uci.edu/ml]. Irvine, CA: University of California, School of Information and Computer Science.
+    [Car Evaluation] https://archive.ics.uci.edu/ml/datasets/Car+Evaluation
+    '''
+    dataset = []
+    featNames = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety']
+    with open('./car/car.data') as df:
+        for line in df:
+            fields = line.strip().split(',')
+            dataset.append(fields)
+    return np.array(dataset), featNames
+
+
+def testDT(alpha=0.):
+    '''
+    测试决策树
+    '''
+    dataSet, featNames = loadCarData()
+    dataIdxList = range(dataSet.shape[0])
+    featIdxList = range(dataSet.shape[1] - 1)
+    np.random.shuffle(dataIdxList)
+    trainCount = len(dataIdxList) * 2 / 3
+    trainIdxList = dataIdxList[0:trainCount]
+    testIdxList = dataIdxList[trainCount:]
+    trainDataSet = dataSet[trainIdxList]
+    testDataSet = dataSet[testIdxList]
+
+    dt = createDecisionTree(trainDataSet, featIdxList, ID3)
+    pruneDT(dt, alpha)
+    errnum = 0.
+    for testData in testDataSet:
+        cls = classify(dt, testData[:-1])
+        if cls != testData[-1]:
+            errnum += 1
+    errRate = errnum / testDataSet.shape[0]
+    print ('test count is %d\nerror count is %d\nerror rate is %f') % \
+            (testDataSet.shape[0], errnum, errRate)
+    return errRate
 
 
 if __name__ == '__main__':
-    myDat, labels = createTestDataSet()
-    myTree = createDecisionTreeID3(np.array(myDat), labels[:])
-    print myTree
-    testLabel = classify(myTree, labels[:], [1,0])
-    print '[1,0] label is %s' % testLabel
+    errRate = 0.
+    for i in range(100):
+        errRate += testDT(0.)
+    print ('average error rate is %f') % (errRate / i)
 
